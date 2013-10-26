d2f23194326809c335a63c371fe9231afab39ed7
otsukano <otsukanoboru@next-group.jp>
2013-10-26 21:38:43 +0900

    [miniblog]setup.sh update.

--- ddl/test.sql
+++ ddl/test.sql
@@ -1,7 +1,6 @@
 GRANT ALL ON miniblog.* to miniblog@localhost;
 SET PASSWORD FOR miniblog@localhost=password('minimini');
 CREATE DATABASE データベース名 CHARACTER SET utf8;
-
 CREATE TABLE user(
     id INTEGER AUTO_INCREMENT,
     user_name VARCHAR(20) NOT NULL,
@@ -10,13 +9,11 @@ CREATE TABLE user(
     PRIMARY KEY(id),
     UNIQUE KEY user_name_index(user_name)
 ) ENGINE = INNODB;
-
 CREATE TABLE following(
     user_id INTEGER,
     following_id INTEGER,
     PRIMARY KEY(user_id, following_id)
 ) ENGINE = INNODB;
-
 CREATE TABLE status(
     id INTEGER AUTO_INCREMENT,
     user_id INTEGER NOT NULL,
@@ -25,11 +22,9 @@ CREATE TABLE status(
     PRIMARY KEY(id),
     INDEX user_id_index(user_id)
 ) ENGINE = INNODB;
-
 ALTER TABLE following ADD FOREIGN KEY (user_id) REFERENCES user(id);
 ALTER TABLE following ADD FOREIGN KEY (following_id) REFERENCES user(id);
 ALTER TABLE status ADD FOREIGN KEY (user_id) REFERENCES user(id);
-
 CREATE TABLE testing(
     id INTEGER AUTO_INCREMENT,
     user_name VARCHAR(20) NOT NULL,
--- setup.sh
+++ setup.sh
@@ -6,6 +6,9 @@ DIR=$(cd $(dirname $0); pwd)
 # テーブル作成SQL
 SQL_TEST=$DIR/ddl/test.sql
 
+# MySQLをバッチモードで実行するコマンド
+CMD_MYSQL="mysql -u otsukano -posukano "
+
 # SQLを読み込んで実行
 mysql -h localhost -u miniblog -pminimini miniblog < $SQL_TEST
 
