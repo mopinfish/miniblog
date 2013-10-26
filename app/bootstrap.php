dc9c5a2782459271e6dcb9759e4fb65f0e64f955
otsukano <otsukanoboru@next-group.jp>
2013-10-26 23:08:41 +0900

    [miniblog]vendorへのパスを修正

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-if (file_exists($loaderFile = dirname(__FILE__) . 'core/ClassLoader.php')) {
+if (file_exists($loaderFile = dirname(__FILE__) . '/core/ClassLoader.php')) {
     require_once $loaderFile;
     $loader = new ClassLoader();
     $loader->registerDir(dirname(__FILE__) . '/core');
@@ -9,7 +9,7 @@ if (file_exists($loaderFile = dirname(__FILE__) . 'core/ClassLoader.php')) {
     $loader->registerDir(dirname(__FILE__) . '/models/biz');
     $loader->register();
 }
-if (file_exists($twigLoaderFile = dirname(__FILE__) . '../vender/Twig/Autoloader.php')) {
-    require_once $twigLoaderFIle;
+if (file_exists($twigLoaderFile = dirname(__FILE__) . '/vendor/Twig/Autoloader.php')) {
+    require_once $twigLoaderFile;
     Twig_Autoloader::register();
 }
