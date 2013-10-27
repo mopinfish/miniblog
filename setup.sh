1f5f6fcded0b9e73085c0a482af0f6858970c44a
otsukano <otsukanoboru@next-group.jp>
2013-10-27 15:17:34 +0900

    [miniblog]bug fixed in layout template.

--- setup.sh
+++ setup.sh
@@ -12,6 +12,11 @@ pwd
 ln -s "../../simply/application/core" ${DIR}/app/core
 ln -s "../../simply/vendor" ${DIR}/app/vendor
 
+# twigキャッシュ用ディレクトリ作成
+mkdir ${DIR}/app/cache/
+chown -R daemon:wheel ${DIR}/app/cache/
+chmod -R 775 ${DIR}/app/cache/
+
 # テーブル作成SQL
 SQL_TEST=$DIR/ddl/test.sql
 
